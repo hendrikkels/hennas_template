9442835d111e35c6436892e9440074d8e737bb3d
Hendrik van Heerden <hendrikkels@icloud.com>
2023-02-15 14:14:44 +0200

    All apis fixed

--- src/pages/api/auth/refresh-token.ts
+++ src/pages/api/auth/refresh-token.ts
@@ -3,36 +3,26 @@ import { createAccessToken, sendRefreshToken, createRefreshToken } from '@/utils
 import cookie from 'cookie'
 import prisma from '../../../../lib/prisma'
 import { NextApiRequest, NextApiResponse } from 'next'
-import { getUserById } from '@/services/auth.service'
+import { getUserById } from '@/services/user.service'
 
 //TODO: Possibly fix api routes
 export default async function refresh_token(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
-        console.log('running refresh token');
         if (!req.headers.cookie) return res.status(400).json({ ok: false, accessToken: '' });
-        console.log('Has Cookie')
         const getToken = cookie.parse(req.headers.cookie);
-        console.log(JSON.stringify(getToken, null, 2));
         const token = getToken.refreshToken;
-
-        console.log('no token');
 
         if (!token) return res.status(400).json({ ok: false, accessToken: '' });
 
-        console.log('has token');
         let payload = null;
 
         if (!process.env.REFRESH_TOKEN_SECRET) {
-            console.error('No value for REFRESH_TOKEN_SECRET found')
             return res.status(400).json({ ok: false, accessToken: '' });
         }
-
-        console.log('before try')
 
         try {
             payload = verify(token, process.env.REFRESH_TOKEN_SECRET)
 
-            console.log(payload);
             if (typeof payload === 'string' || payload instanceof String) {
                 return res.status(400).json({ ok: false, accessToken: '' });
             }
