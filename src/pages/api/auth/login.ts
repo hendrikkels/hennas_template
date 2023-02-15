34f9f8b102cfe01c024afddf01f3e2b5512318df
Hendrik van Heerden <hendrikkels@icloud.com>
2023-02-15 14:11:25 +0200

    Fixed error messages

--- src/pages/api/auth/login.ts
+++ src/pages/api/auth/login.ts
@@ -8,27 +8,30 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (req.method === 'POST') {
         const body = JSON.parse(req.body);
 
+        // Check if body contains both email and password
         if (!body.email || !body.password) {
             return res.status(400).json({ error: 'Invalid Credentials' });
         }
 
         const { email, password } = body;
 
+        // Get only the password and id of the user
         const loginUser = await loginUserByEmail(email);
 
-        if (!loginUser) return res.status(400).json({ error: 'A user with this email does not exist!' });
+        if (!loginUser) return res.status(400).json({ error: 'Email not found' });
 
+        // Validate the password
         const valid = await validatePassword(password, loginUser?.password ?? '');
 
         if (valid) {
             const token = createRefreshToken(loginUser);
-            if (!token) return res.status(400).json({ error: 'Something went wrong!' });
+            if (!token) return res.status(400).json({ error: 'Something went wrong' });
             const accessToken = createAccessToken(loginUser)
             sendRefreshToken(res, token)
             const user = await getUser(loginUser.id);
             res.status(200).json({ user: user, accessToken });
         } else {
-            res.status(404).json({ error: 'Password incorrect wrong!' });
+            res.status(404).json({ error: 'Incorrect Password' });
         }
     }
 }
--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -16,11 +16,11 @@ export default async function handler(
 
     //Check if email exists
     const emailExists = await getUserByEmail(body.email);
-    if (emailExists) return res.status(403).json({ error: `Email already in use!` });
+    if (emailExists) return res.status(403).json({ error: `Email already in use` });
 
     //Check if username exists
     const usernameExists = await getUserByUsername(body.username);
-    if (usernameExists) return res.status(403).json({ error: `Username already in use!` });
+    if (usernameExists) return res.status(403).json({ error: `Username already in use` });
 
     //Hash the password
     const hashedPassword = await hashPassword(body.password);
